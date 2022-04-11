1a1d3a07fce4d269bfa67b9bb4a8f04b810eff1a
anderson <deveil2825@163.com>
2022-04-11 11:57:08 +0800

    滚动
    滚动

--- mBanner/carousel2.js
+++ mBanner/carousel2.js
@@ -16,11 +16,11 @@
         var banner = document.querySelector(self.el),
             slide = banner.querySelector(".slide"),
             startPointX = 0,
+            startPointY=0,
+            endX=0,
+            endY=0,
             startLeft = 0,
             movePointX = 0,
-            startPointY = 0,
-            endPointX = 0,
-            endPointY = 0,
             cn = 0,
             timer = null,
             imgWidth = banner.offsetWidth;
@@ -33,7 +33,7 @@
         var len = slide.children.length;
         paginationTotal ? paginationTotal.innerText = len / 2 : paginationTotal
         slide.style.width = len * banner.offsetWidth + "px";
-        slide.style.transform = "translate3d(0,0,0)";
+        slide.style.transform = "translateX(0px)";
         if(len<=2){
             pagination.remove();
             return false;
@@ -47,29 +47,27 @@
             if (cn == len - 1) {
                 cn = len / 2 - 1;
             }
-            slide.style.transform = "translate3d(" + -cn * imgWidth + "px,0,0)";
+            slide.style.transform = "translateX(" + -cn * imgWidth + "px)";
             startPointX = ev.changedTouches[0].pageX;
-            startPointY = ev.changedTouches[0].pageY;
             startLeft = parseFloat(slide.style.transform.split("(")[1]);
         });
         banner.addEventListener("touchmove", function (ev) {
-            movePointX = ev.changedTouches[0].pageX - startPointX;
-            endPointX =  ev.changedTouches[0].pageX;
-            endPointY =  ev.changedTouches[0].pageY;
-            if(Math.abs(startPointX - endPointX ) > Math.abs(startPointY - endPointY )){
+            endX = ev.changedTouches[0].pageX;
+            endY = ev.changedTouches[0].pageY;
+            if(Math.abs(startPointX - endX) > Math.abs(startPointY - endY)){
                 ev.preventDefault();
             }
-            slide.style.transform = "translate3d(" + (movePointX + startLeft) + "px,0,0)";
+            movePointX = ev.changedTouches[0].pageX - startPointX;
+            slide.style.transform = "translateX(" + (movePointX + startLeft) + "px)";
         });
         banner.addEventListener("touchend", function (ev) {
             movePointX = ev.changedTouches[0].pageX - startPointX;
-            var backWidth = banner.offsetWidth/2;
-            console.log(movePointX,banner.offsetWidth,backWidth,imgWidth);
+            var backWidth = imgWidth / len;
             if (Math.abs(movePointX) > backWidth) {
                 cn -= movePointX / Math.abs(movePointX);
             }
             slide.style.transition = ".3s";
-            slide.style.transform = "translate3d(" + -cn * imgWidth + "px,0,0)";
+            slide.style.transform = "translateX(" + -cn * imgWidth + "px)";
             var hn = cn % (len / 2);
             paginationCurrent.innerText = hn + 1;
             self.autoplay ? timer = setInterval(move, 4000) : self.autoplay;
@@ -80,7 +78,7 @@
                 cn = len - 1;
             }
             slide.style.transition = '.3s';
-            slide.style.transform = "translate3d(" + -cn * imgWidth + "px,0,0)";
+            slide.style.transform = "translateX(" + -cn * imgWidth + "px)";
             setTimeout(function () {
                 if (cn == len - 1) {
                     cn = len / 2 - 1;
@@ -88,7 +86,7 @@
                 } else {
                     slide.style.transition = ".3s";
                 }
-                slide.style.transform = "translate3d(" + -cn * imgWidth + "px,0,0)";
+                slide.style.transform = "translateX(" + -cn * imgWidth + "px)";
 
             },100)
             var hn = cn % (len / 2);
